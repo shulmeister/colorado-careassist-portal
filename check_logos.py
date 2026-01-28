ae1d5b1c434dfbb52dec0dedc2b25eb5249c56cd
Jason Shulmeister <jason@coloradocareassist.com>
2026-01-27 22:59:29 -0700

    Add logo check script

--- check_logos.py
+++ check_logos.py
@@ -1,7 +1,23 @@
-from portal_database import db_manager
-from portal_models import PortalTool
-db = db_manager.get_session()
-tools = db.query(PortalTool).filter(PortalTool.name.in_(['Google Ads', 'Predis.ai', 'Intuit QuickBooks'])).all()
-for t in tools:
-    print(f'{t.name}: {t.icon_url}')
-db.close()
+#!/usr/bin/env python3
+"""Check current logo URLs in database"""
+import os
+from dotenv import load_dotenv
+from portal.portal_database import db_manager
+from portal.portal_models import PortalTool
+
+load_dotenv()
+
+def check_logos():
+    db = db_manager.get_session()
+    try:
+        tools = db.query(PortalTool).filter(
+            PortalTool.name.in_(['EbizCharge', 'Fax.Plus', 'CBI InstaCheck', 'CAPS', 'Google Admin', 'AI Tools', 'RingCentral'])
+        ).all()
+
+        for tool in tools:
+            print(f"{tool.name}: {tool.icon}")
+    finally:
+        db.close()
+
+if __name__ == "__main__":
+    check_logos()
